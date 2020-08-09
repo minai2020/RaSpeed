f498adba751cd3befa340b3c72a4ec65e1feb77a
probize <52457429+probize@users.noreply.github.com>
2020-08-09 02:17:45 +0200

    Add files via upload
    rewrite the most, now crone function and task handler. JSON parser and timeout for docker call and as well a check if perf server do not reply back

--- RaSpeed.go
+++ RaSpeed.go
@@ -1,163 +1,404 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"github.com/Sirupsen/logrus"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"net"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/robfig/cron"
+	log "github.com/sirupsen/logrus"
+	yaml "gopkg.in/yaml.v2"
 )
 
+// Config is yaml rep of the config file there is loaded in
 type Config struct {
-	IperfImg       string    `yaml:"docker-image"`
-	TestDuration   string    `yaml:"test-length"`
-	TestInterval   string    `yaml:"test-interval"`
-	ServerPort     string    `yaml:"server-port"`
-	TsdbServer     string    `yaml:"grafana-address"`
-	TsdbPort       string    `yaml:"grafana-port"`
-	TsdbDownPrefix string    `yaml:"tsdb-download-prefix"`
-	TsdbUpPrefix   string    `yaml:"tsdb-upload-prefix"`
-	Entry          []Servers `yaml:"iperf-servers"`
+	UseDocker          string `yaml:"use-docker"`
+	DockerImage        string `yaml:"docker-image"`
+	GrafanaAddress     string `yaml:"grafana-address"`
+	GrafanaPort        string `yaml:"grafana-port"`
+	TsdbDownloadPrefix string `yaml:"tsdb-download-prefix"`
+	TsdbUploadPrefix   string `yaml:"tsdb-upload-prefix"`
+	LoggingFile        string `yaml:"logging-File"`
+	Servers            []struct {
+		HostID     string `yaml:"Host-ID"`
+		ServerIP   string `yaml:"server-IP"`
+		ServerPort string `yaml:"server-port"`
+		Testlength int    `yaml:"testlength"`
+		Crontask   string `yaml:"crontask"`
+		Parallel   int    `yaml:"parallel"`
+	} `yaml:"Servers"`
 }
 
-type Servers map[string]string
-
-type Endpoint struct {
-	ServerIP   string
-	Port       string
-	ServerName string
+// Iperf3Json type is export from
+type Iperf3Json struct {
+	Start struct {
+		Connected []struct {
+			LocalHost  string `json:"local_host"`
+			RemoteHost string `json:"remote_host"`
+		} `json:"connected"`
+		Version    string `json:"version"`
+		SystemInfo string `json:"system_info"`
+		Timestamp  struct {
+			Time     string `json:"time"`
+			Timesecs int    `json:"timesecs"`
+		} `json:"timestamp"`
+		ConnectingTo struct {
+			Host string `json:"host"`
+			Port int    `json:"port"`
+		} `json:"connecting_to"`
+		TCPMssDefault int `json:"tcp_mss_default"`
+		TestStart     struct {
+			Protocol   string `json:"protocol"`
+			NumStreams int    `json:"num_streams"`
+			Blksize    int    `json:"blksize"`
+			Omit       int    `json:"omit"`
+			Duration   int    `json:"duration"`
+			Bytes      int    `json:"bytes"`
+			Blocks     int    `json:"blocks"`
+			Reverse    int    `json:"reverse"`
+			Tos        int    `json:"tos"`
+		} `json:"test_start"`
+	} `json:"start"`
+	Intervals []struct {
+		Streams []struct {
+			Socket        int     `json:"socket"`
+			Start         int     `json:"start"`
+			End           float64 `json:"end"`
+			Seconds       float64 `json:"seconds"`
+			Bytes         int     `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+			Omitted       bool    `json:"omitted"`
+			Sender        bool    `json:"sender"`
+		} `json:"streams"`
+		Sum struct {
+			Start         int     `json:"start"`
+			End           float64 `json:"end"`
+			Seconds       float64 `json:"seconds"`
+			Bytes         int     `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+			Omitted       bool    `json:"omitted"`
+			Sender        bool    `json:"sender"`
+		} `json:"sum"`
+	} `json:"intervals"`
+	End struct {
+		Streams []struct {
+			Sender struct {
+				Socket        int     `json:"socket"`
+				Start         int     `json:"start"`
+				End           float64 `json:"end"`
+				Seconds       float64 `json:"seconds"`
+				Bytes         int     `json:"bytes"`
+				BitsPerSecond float64 `json:"bits_per_second"`
+				Sender        bool    `json:"sender"`
+			} `json:"sender"`
+			Receiver struct {
+				Socket        int     `json:"socket"`
+				Start         int     `json:"start"`
+				End           float64 `json:"end"`
+				Seconds       float64 `json:"seconds"`
+				Bytes         int     `json:"bytes"`
+				BitsPerSecond float64 `json:"bits_per_second"`
+				Sender        bool    `json:"sender"`
+			} `json:"receiver"`
+		} `json:"streams"`
+		SumSent struct {
+			Start         int     `json:"start"`
+			End           float64 `json:"end"`
+			Seconds       float64 `json:"seconds"`
+			Bytes         int     `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+			Sender        bool    `json:"sender"`
+		} `json:"sum_sent"`
+		SumReceived struct {
+			Start         int     `json:"start"`
+			End           float64 `json:"end"`
+			Seconds       float64 `json:"seconds"`
+			Bytes         int     `json:"bytes"`
+			BitsPerSecond float64 `json:"bits_per_second"`
+			Sender        bool    `json:"sender"`
+		} `json:"sum_received"`
+		CPUUtilizationPercent struct {
+			HostTotal    float64 `json:"host_total"`
+			HostUser     float64 `json:"host_user"`
+			HostSystem   float64 `json:"host_system"`
+			RemoteTotal  float64 `json:"remote_total"`
+			RemoteUser   float64 `json:"remote_user"`
+			RemoteSystem float64 `json:"remote_system"`
+		} `json:"cpu_utilization_percent"`
+		ReceiverTCPCongestion string `json:"receiver_tcp_congestion"`
+	} `json:"end"`
+	Error string `json:"error"`
 }
 
+// Cli ommandline options
 type Cli struct {
 	Debug      bool
 	ConfigPath string
 	Help       bool
+	LogFile    string
+	LogLevel   string
 }
 
 var cli *Cli
-var iperfImg = "networkstatic/iperf3"
-var log = logrus.New()
-
-func SetLogger(l *logrus.Logger) {
-	log = l
-}
+var config Config
+var mutex = &sync.Mutex{}
 
 func init() {
+
 	const (
-		debugFlag     = false
-		debugDescrip  = "Run in debug mode to display all shell commands being executed"
-		configPath    = "./config.yml"
-		configDescrip = "Path to the configuration file -config=path/config.yml"
-		helpFlag      = false
-		helpDescrip   = "Print Usage Options"
+		debugFlag       = true
+		debugDescrip    = "Run in debug mode to display all shell commands being executed"
+		configPath      = "./config.yaml"
+		configDescrip   = "Path to the configuration file -config=path/config.yml"
+		helpFlag        = false
+		helpDescrip     = "Print Usage Options (this)"
+		logFile         = "logfile.log"
+		logFlagDescrip  = "Log to file "
+		logLevel        = "Trace"
+		logLevelDescrip = "Trace, Debug, Info, Warning, Error, Fatal, Panic"
 	)
 	cli = &Cli{}
 	flag.BoolVar(&cli.Debug, "debug", debugFlag, debugDescrip)
 	flag.StringVar(&cli.ConfigPath, "config", configPath, configDescrip)
 	flag.BoolVar(&cli.Help, "help", helpFlag, helpDescrip)
+	flag.StringVar(&cli.LogLevel, "Loglevel", logLevel, logFlagDescrip)
+	flag.StringVar(&cli.LogFile, "logfile", logFile, logFlagDescrip)
+
+	if cli.Debug {
+		log.WithFields(log.Fields{
+			"Debug":      cli.Debug,
+			"Loglevel":   logLevel,
+			"ConfigPath": configPath,
+		}).Info(debugDescrip)
+
+	}
+
+	level, err := log.ParseLevel(cli.LogLevel)
+	if err != nil {
+		log.Warn("Cannot parse conf level, default to INFO")
+	} else {
+		log.Println(level)
+		log.SetLevel(level)
+	}
+	if cli.LogFile != "" {
+		log.Info("Logging will continue on file : " + cli.LogFile)
+		f, err := os.OpenFile(cli.LogFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			fmt.Printf("error opening file: %v", err)
+		}
+
+		log.SetOutput(f)
+	}
+
 }
 
 func main() {
-	flag.Parse()
-	if cli.Help {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	for {
-		// Read in the yaml configuration from config.yaml
-		data, err := ioutil.ReadFile(cli.ConfigPath)
-		if err != nil {
-			log.Fatalln("There was a problem opening the configuration file. Make sure "+
-				"'config.yml' is located in the same directory as the binary 'cbandwidth' or set"+
-				" the location using -config=path/config.yml || Error: ", err)
-		}
-		config := Config{}
-		if err := yaml.Unmarshal([]byte(data), &config); err != nil {
-			log.Fatal(err)
-		}
-		graphiteSocket := net.JoinHostPort(config.TsdbServer, config.TsdbPort)
-		for _, val := range config.Entry {
-			for endpointAddress, endpointName := range val {
-				if endpointName == "" {
-					endpointName = endpointAddress
-				}
-				// Test the download speed to the iperf endpoint
-				iperfDownResults, err := runCmd(fmt.Sprintf("docker run -i --rm %s -P 1 -t %s -f m "+
-					"-p %s -c %s | tail -n 3 | head -n1 | awk '{print $7}'",
-					config.IperfImg,
-					config.TestDuration,
-					config.ServerPort,
-					endpointAddress,
-				), cli)
-				if strings.Contains(iperfDownResults, "error") {
-					log.Errorf("Error testing iperf server at %s:%s", endpointAddress, config.ServerPort)
-					log.Errorf("Verify iperf is running and reachable at %s:%s", endpointAddress, config.ServerPort)
-					log.Errorln(err, iperfDownResults)
-				} else {
-					// Write the download results to the tsdb
-					log.Infof("Download results for endpoint %s -> %s Mbps", endpointAddress, iperfDownResults)
-					timeDownNow := time.Now().Unix()
-					sendGraphite("tcp", graphiteSocket, fmt.Sprintf("%s.%s %s %d\n",
-						config.TsdbDownPrefix, endpointName, iperfDownResults, timeDownNow))
-				}
-				// Test the upload speed to the iperf endpoint
-				iperfUpResults, err := runCmd(fmt.Sprintf("docker run -i --rm %s -P 1 -R -t %s "+
-					"-f m  -p %s -c %s | tail -n 3 | head -n1 | awk '{print $7}'",
-					config.IperfImg,
-					config.TestDuration,
-					config.ServerPort,
-					endpointAddress,
-				), cli)
-				if strings.Contains(iperfUpResults, "error") {
-					log.Errorf("Error testing iperf server at %s:%s", endpointAddress, config.ServerPort)
-					log.Errorf("Verify iperf is running and reachable at %s:%s", endpointAddress, config.ServerPort)
-					log.Errorln(err, iperfUpResults)
-				} else {
-					// Write the upload results to the tsdb
-					log.Infof("Upload results for endpoint %s -> %s Mbps", endpointAddress, iperfUpResults)
-					timeUpNow := time.Now().Unix()
-					sendGraphite("tcp", graphiteSocket, fmt.Sprintf("%s.%s %s %d\n",
-						config.TsdbUpPrefix, endpointName, iperfUpResults, timeUpNow))
-				}
-			}
-		}
-		// Polling interval as defined in the config file. The default is 5 minutes.
-		t, _ := time.ParseDuration(string(config.TestInterval) + "s")
-		time.Sleep(t)
-	}
-}
 
-// Run the iperf container and return the output and any errors
-func runCmd(command string, cli *Cli) (string, error) {
-	command = strings.TrimSpace(command)
-	var cmd string
-	var args []string
-	cmd = "/bin/bash"
-	args = []string{"-c", command}
-	// log the shell command being run to stdout if the debug flag is set
-	if cli.Debug {
-		log.Infoln("Running shell command -> ", args)
-	}
-	output, err := exec.Command(cmd, args...).CombinedOutput()
-	return strings.TrimSpace(string(output)), err
-}
+	println("Program start")
 
-// Write the results to a graphite socket
-func sendGraphite(connType string, socket string, msg string) {
-	conn, err := net.Dial(connType, socket)
+	data, err := ioutil.ReadFile(cli.ConfigPath)
 	if err != nil {
-		log.Errorf("Could not connect to the graphite server -> %s", socket)
-		log.Errorf("Verify the graphite server is running and reachable at %s", socket)
+		log.Fatalln("There was a problem opening the configuration file. Make sure "+
+			"'config.yml' is located in the same directory as the binary 'raspeed' or set"+
+			" the location using -config=path/config.yml || Error: ", err)
+	}
+	//config := Config{}
+
+	if err := yaml.Unmarshal([]byte(data), &config); err != nil {
+		log.Fatal(err)
+	}
+	println("Servers the wille be tested. ")
+
+	for index := range config.Servers {
+
+		log.WithFields(log.Fields{
+			"Index":       index,
+			"Cron":        config.Servers[index].Crontask,
+			"Host ID":     config.Servers[index].HostID,
+			"Server IP":   config.Servers[index].ServerIP,
+			"Server Port": config.Servers[index].ServerPort,
+			"Parallel":    config.Servers[index].Parallel,
+			"Test in sec": config.Servers[index].Testlength,
+		}).Info("Adding host to task list")
+
+		cronDockeDuplex(config.Servers[index].Crontask,
+			config.DockerImage,
+			config.Servers[index].ServerIP,
+			config.Servers[index].ServerPort,
+			config.Servers[index].Parallel,
+			config.Servers[index].Testlength,
+			config.Servers[index].HostID)
+
+	}
+
+	println("Out of Cron assigment")
+	time.Sleep(time.Minute * 3)
+	println("Program should have ended")
+	//fmt.Printf("%v ", v.NumField())
+
+	/**
+	up, down := runDockerIperfDuplex("probez/iperf3", "speedtest.hiper.dk", 3, 5)
+
+	grafUpload("192.168.10.200", "2003", "bandwidth.upload", "Realhost", down)
+	grafUpload("192.168.10.200", "2003", "bandwidth.download", "Realhost", up)
+	fmt.Println("Program is exited")
+	*/
+
+	/*****-----
+
+		fmt.Println("Program is in progress")
+		log.Println("STARTIGN CRON PROCESS")
+	  /**/
+	//cronDockeDuplex("*/1 * * * *", "probez/iperf3", "speedtest.hiper.dk", "5201", 3, 5, "hostid-update")
+
+	//time.Sleep(time.Minute * 3)
+}
+
+func cronDockeDuplex(timestring string, dockerimage string, server string, port string, parallel int, howlong int, hostid string) {
+	c := cron.New()
+	c.AddFunc(timestring, func() { runDockerDuplex(dockerimage, server, port, parallel, howlong, hostid) })
+	c.Start()
+
+}
+
+// RunDockerDuplex will run test both ways
+func runDockerDuplex(dockerimage string, server string, port string, parallel int, howlong int, hostid string) {
+	up, down := runDockerIperfDuplex(dockerimage, server, port, parallel, howlong)
+
+	grafUpload(config.GrafanaAddress, config.GrafanaPort, config.TsdbDownloadPrefix, hostid, down)
+	grafUpload(config.GrafanaAddress, config.GrafanaPort, config.TsdbUploadPrefix, hostid, up)
+
+}
+
+func runDockerIperfDuplex(dockerimage string, server string, port string, parallel int, howlong int) (upload float64, download float64) {
+
+	var uploadresult, err = runDockerIperf(dockerimage, server, port, false, parallel, howlong)
+	if err != nil {
+		log.Error("Upload failed")
+	}
+	upload = (uploadresult.End.SumReceived.BitsPerSecond / 1000000)
+
+	log.Printf("Upload speed meassured : %.2f Mbit/s", upload)
+
+	var downloadresult, erro = runDockerIperf(dockerimage, server, port, true, parallel, howlong)
+	if erro != nil {
+		log.Error("Download failed")
+	}
+	download = (downloadresult.End.SumReceived.BitsPerSecond / 1000000)
+	log.Printf("Download speed meassured : %.2f Mbit/s", download)
+
+	return upload, download
+
+}
+
+func grafUpload(graphitehost string, graphiteport string, graphiteTableprefix string, graphiteendpointname string, result float64) {
+	graphiteSocket := net.JoinHostPort(graphitehost, graphiteport)
+	timeNow := time.Now().Unix()
+	uploadResults := fmt.Sprintf("%3f", result)
+	msg := fmt.Sprintf("%s.%s %s %d\n", graphiteTableprefix, graphiteendpointname, uploadResults, timeNow)
+	log.WithFields(log.Fields{
+		"graphiteTableprefix":  graphiteTableprefix,
+		"graphiteendpointname": graphiteendpointname,
+		"Upload results":       uploadResults,
+		"time (local)":         timeNow,
+	}).Trace("Function grafUpload called with")
+
+	log.Trace("String to update : ", msg)
+	conn, err := net.Dial("tcp", graphiteSocket)
+
+	if err != nil {
+
+		log.Printf("Could not connect to the graphite server @%s", graphiteSocket)
+		log.Printf("Verify the graphite server is running and reachable at %s", graphitehost)
 	} else {
 		defer conn.Close()
 		_, err = fmt.Fprintf(conn, msg)
+
+		log.WithFields(log.Fields{
+			"Remote Host": conn.RemoteAddr(),
+			"msg":         msg,
+		}).Info("Success uploading to Graphite host")
+
 		if err != nil {
-			log.Errorf("Error writing to the graphite server at -> %s", socket)
+			log.Printf("Error writing to the graphite server at -> %s", graphiteSocket)
 		}
+
 	}
+}
+
+func runDockerIperf(dockerimage string, server string, port string, reverse bool, parallel int, howlong int) (iperfResult Iperf3Json, err error) {
+	//lets get some silince
+	log.Trace("Mutex lock")
+	mutex.Lock()
+
+	data := Iperf3Json{}
+	path, err := exec.LookPath("docker")
+	if err != nil {
+		log.Fatalln("installing docker is needed")
+	}
+
+	var res string
+
+	if reverse {
+		res = "-R"
+	}
+
+	timeout := time.Duration(int64(howlong*2)) * time.Second
+
+	log.Printf("Function will be called with a timeout of :%v", (time.Duration(int64(howlong*2)) * time.Second))
+
+	// Create a new context and add a timeout so command will timeout after 2*test duration
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel() // The cancel should be deferred so resources are cleaned up	// Create a new context and add a timeout to it
+
+	//cmd := exec.CommandContext(ctx, "ping", "-c 4", "-i 1", "8.8.8.8")
+	cmd := exec.CommandContext(ctx, path, "run", "-i", "--rm", dockerimage, "-c", server, "-J", res, "-P", strconv.Itoa(parallel), "-p", port, "-t", strconv.Itoa(howlong))
+	// Relax off again
+
+	log.Trace(cmd)
+	output, err := cmd.Output()
+
+	if err != nil {
+
+		if strings.Contains(err.Error(), "125") {
+			log.Fatal("Docker is not running !!")
+		}
+
+		if ctx.Err() == context.DeadlineExceeded {
+
+			log.Error("Command failed do to time out after")
+			return data, errors.New("Command failed do to time out after")
+		}
+		// It was a fail, most likely because of busy serer
+		// Error is also comminig on Iperf3Json
+		_ = json.Unmarshal(output, &data)
+
+		log.Error("IPerf server: " + server + " reported back :" + data.Error)
+
+		err = errors.New(data.Error)
+
+		//	fmt.Printf("Return data: %s\n", output)
+		log.Trace("Mutex Unlock")
+		mutex.Unlock()
+		return data, err
+
+	}
+
+	_ = json.Unmarshal(output, &data)
+
+	//fmt.Printf("Return data: %s\n", output)
+	log.Trace("Remove Mutex")
+	mutex.Unlock()
+	return data, err
+
 }
